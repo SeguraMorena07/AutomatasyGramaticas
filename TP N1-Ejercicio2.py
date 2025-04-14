33dfd4ac2560b88f8539d41610edb084b5437bd4
Caro-D54 <c.donaire@alumno.um.edu.ar>
2025-04-14 19:15:10 -0300

    Correccion ejercicio 2_TP 1

--- TP N1-Ejercicio2.py
+++ TP N1-Ejercicio2.py
@@ -1,12 +1,29 @@
 def solve():
     operacion = input("Ingrese la operación matemática (solo números, '+' y '*'): ")
     try:
-        # Validar que solo contenga números, '+' o '*'
+
         if not all(char.isdigit() or char in "+* " for char in operacion):
             return "Error: operación inválida. Usa solo números, '+' y '*'."
-        resultado = eval(operacion)
-        return int(resultado)
-    except Exception:
+        
+
+        operacion = operacion.replace(" ", "")
+
+        sumandos = operacion.split('+')
+        total = 0
+        
+        for sumando in sumandos:
+            factores = sumando.split('*')
+            producto = 1
+            for factor in factores:
+                if not factor.isdigit():
+                    return "Error: operación inválida, formato incorrecto."
+                
+                producto *= int(factor)
+            
+            total += producto
+
+        return total
+    except Exception as e:
         return "Error: ocurrió un problema al procesar la operación."
 
 print(solve())
